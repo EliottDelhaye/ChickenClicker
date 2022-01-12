3e9486798da538e9f4826968a2a4eae8dbd85192
EliottDELHAYE <91311354+EliottDELHAYE@users.noreply.github.com>
2022-01-12 16:52:12 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -5,6 +5,7 @@
 /*   | |___  | | | | | | | |___  | | \ \  | |___  | | \  |      | |___  | |___  | | | |___  | | \ \  | |___  | | \ \   */ /*  | |___   ___| |  */
 /*   \_____| |_| |_| |_| \_____| |_|  \_\ |_____| |_|  \_|      \_____| |_____| |_| \_____| |_|  \_\ |_____| |_|  \_\  */ /*  |_____| |_____|  */
 
+
 let click = 0;
 
 function Click(){
@@ -17,8 +18,27 @@ let nugget = 0;
 function ClickNugget(){
    nugget++;
    document.getElementById("nugget").innerHTML = nugget;
+   if (nugget  === 30){
+    var confirmation = confirm("Félicitation !!! Vous avez débloqué le mode seau Nugget. Voulez vous l'activer ? ");
+    if (confirmation){
+    //action à faire pour la valeur true
+        var scoreTriple ="Mode seau Nugget activé avec succès. Votre score sera triplé par click";
+    alert(scoreTriple);
+        poulet = 0;
+        nugget +=3;
+    }else{
+    //action à faire pour la valeur false
+    poulet = poulet;
+    }
+}
 }
 
+let poulet = 0
+function NoClickPoulet(){
+    poulet++;
+    document.getElementById("poulet").innerHTML = poulet;
+ }
+ 
 let targetImage = "poulet";
 
 function changeImage(){
@@ -26,9 +46,32 @@ function changeImage(){
     if(targetImage=="poulet"){
         image.src = "Photos/Bucket.png";
         targetImage = "bucket";
+        
+        
     }
     else{
         image.src = "Photos/Poulet.png";
         targetImage = "poulet";
+        noPressEnter(document.body);
     }
 }
+
+function bruit(){
+    var bruit = new Audio();
+    bruit.src = "Photos/PouletSFX.mp3";
+    bruit.play();
+}
+
+/*
+function plumes(){
+    var plumes = document.getElementById("plumes");
+    if(targetImage =="poulet"){
+        image.src = "Photos/plumes.png";
+        targetImage = "plumes";
+    }
+}
+
+function popupBasique(page) {
+    window.open(page);
+  }
+*/
